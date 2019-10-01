e7bb89a923445cede4197b8820254cfb92328ea0
guacke <Guillaume.Acke@UGent.be>
2019-10-01 17:23:17 +0200

    Add setup.py. Adjust capitalization ghf.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='ghf',
+      version='0.0.1',
+      description='Generalized Hartree-Fock code',
+      author='The Ghent Quantum Chemistry Group',
+      packages=['ghf'],
+      )
