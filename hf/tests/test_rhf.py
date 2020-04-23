33053aafe0eb3721cefc913a995dc6031015f4b8
Xeno De Vriendt <xeno.dv@pandora.be>
2020-04-23 20:04:18 +0200

    rewriting tests

---
+++ hf/tests/test_rhf.py
@@ -0,0 +1,46 @@
+"""
+Testing the RHF method
+=======================
+
+Simple tests to check whether or not the functions return the correct value.
+"""
+from hf.HartreeFock import *
+import numpy as np
+from pyscf import *
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Create the molecules.
+h2o = gto.M(atom='O 0.000000000000 -0.143225816552 0.000000000000;'
+                 'H 1.638036840407 1.136548822547 -0.000000000000;'
+                 'H -1.638036840407 1.136548822547 -0.000000000000',
+            unit='bohr',
+            basis='sto-3g')
+
+
+def test_scf():
+    """
+    Test whether or not the RHF method returns the correct result when using regular scf.
+
+    """
+    x = RHF.MF(h2o, 10)
+    assert np.isclose(-74.94207992819219, x.get_scf_solution()) is True
+
+
+def test_diis():
+    """
+    Test whether or not the RHF method returns the correct result when using diis.
+
+    """
+    x = RHF.MF(h2o, 10)
+    assert np.isclose(-74.94207992819219, x.get_scf_solution_diis()) is True
+
+
+def test_scf_vs_diis():
+    """
+    Test whether or not the RHF method returns the correct result when using regular scf.
+
+    """
+    x = RHF.MF(h2o, 10)
+    assert x.scf(convergence=1e-6)[1] >= x.diis(convergence=1e-6)[1]
